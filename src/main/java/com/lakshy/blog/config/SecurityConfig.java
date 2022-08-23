7b87ffc232283b6a3a519379098a24e4b192b716
Lakshy Gupta <lakshygupta99@gmail.com>
2022-08-23 19:58:32 +0530

    database auth wih roles and user adding comments

--- src/main/java/com/lakshy/blog/config/SecurityConfig.java
+++ src/main/java/com/lakshy/blog/config/SecurityConfig.java
@@ -34,6 +34,13 @@ public class SecurityConfig {
         return http.build();
     }
 		
+	
+	/* 
+	 * === No Need for authenticationManager in Spring Security >= 5.7.2 === 
+	 * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+	 * It is also no longer necessary to manually set UserDetailsService implementation in AuthenticationManager instance, 
+	 * it only needs to exist in the spring context (example is done with the help of annotations to create beans).
+	 */
 //	@Bean
 //    AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder) throws Exception {
 //        return builder.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder()).and().build();
